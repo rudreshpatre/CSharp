700c6dd5d7ef13ca55380d3dfad88d25d58b48e2
rudreshpatre <rudreshpatre@gmail.com>
2019-10-14 11:15:19 +0530

    Added Linear Search

---
+++ dsawithCsharp/SearchingAlgos/LinearSearch/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LinearSearch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
