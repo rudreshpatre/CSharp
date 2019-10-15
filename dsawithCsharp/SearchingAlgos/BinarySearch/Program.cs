5ef9b227799fbce8042f7c9cba15c51bedcddf8e
rudreshpatre <rudreshpatre@gmail.com>
2019-10-15 10:06:25 +0530

    added binary helper

--- dsawithCsharp/SearchingAlgos/BinarySearch/Program.cs
+++ dsawithCsharp/SearchingAlgos/BinarySearch/Program.cs
@@ -6,7 +6,23 @@ namespace BinarySearch
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            try
+            {
+                var arr = BinarySearchHelper.GetArrayList();
+                BinarySearchHelper.PrintArray(arr);
+                arr = BinarySearchHelper.InsertionSort(arr);
+                Console.WriteLine("Enter number you want to search.");
+                var searchKey = Convert.ToInt32(Console.ReadLine());
+                arr = BinarySearchHelper.BinarySearch(arr, searchKey);
+                BinarySearchHelper.PrintArray(arr);
+            }
+            catch (System.Exception ex)
+            {
+                if (ex.Message.Contains("Input string was not in a correct format"))
+                {
+                    Console.WriteLine("Please enusure you are entering number as input.");
+                }
+            }
         }
     }
 }
