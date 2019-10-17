7dbe98e8619fd2780f2a1a871f55cee4f7a2b4ed
rudreshpatre <rudreshpatre@gmail.com>
2019-10-17 11:47:48 +0530

    Added InsertionSort

---
+++ dsawithCsharp/InsertionSort/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace InsertionSort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+             try
+            {
+                var arr = InsertionSortHelper.GetUnsortedList();
+                InsertionSortHelper.PrintArray(arr);
+                arr = InsertionSortHelper.InsertionSort(arr);
+                InsertionSortHelper.PrintArray(arr);
+            }
+            catch (Exception ex)
+            {
+               if(ex.Message.Contains("Input string was not in a correct format")){
+                   Console.WriteLine("Please enusure you are entering number as input.");
+               }
+            }
+        }
+    }
+}
